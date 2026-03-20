cd82427266d71ac3a5bf2bba58c36ee3bbde0b0d
pibba <avenidabrasil284@gmail.com>
2026-03-20 20:27:58 -0300

    Update server.js

--- convertpro-backend/server.js
+++ convertpro-backend/server.js
@@ -1,1 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// rotas
+app.use("/convert", require("./routes/convert"));
+app.use("/pdf", require("./routes/pdf"));
+
+app.get("/", (req, res) => {
+  res.send("🚀 ConvertPro Backend rodando");
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Servidor rodando na porta " + (process.env.PORT || 3000))
+);
